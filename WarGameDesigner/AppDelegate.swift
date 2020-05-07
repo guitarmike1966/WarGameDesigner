17d0404d51999cbe2480efea519903271a2cc370
Mike O'Connell <guitarmike1966@gmail.com>
2020-05-06 22:30:39 -0500

    Initial Commit

---
+++ WarGameDesigner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WarGameDesigner
+//
+//  Created by Michael O'Connell on 5/6/20.
+//  Copyright © 2020 Michael O'Connell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
