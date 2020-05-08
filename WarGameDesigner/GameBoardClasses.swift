7a8dd90a3f319de69232be95c0d477410afcf53d
Mike O'Connell <guitarmike1966@gmail.com>
2020-05-07 23:18:51 -0500

    base functionality

---
+++ WarGameDesigner/GameBoardClasses.swift
@@ -0,0 +1,184 @@
+//
+//  GameBoardClasses.swift
+//  WarGameDesigner
+//
+//  Created by Michael O'Connell on 5/7/20.
+//  Copyright © 2020 Michael O'Connell. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+extension NSColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+
+    static let darkGreen = NSColor(hex: "005000")
+    static let lightGreen = NSColor(hex: "40FF40")
+    static let darkBlue = NSColor(hex: "000090")
+}
+
+
+enum GameBoardCellTerrain {
+    case Woods
+    case Grass
+    case Desert
+    case Tundra
+    case Mountain
+    case Water
+    case Other
+
+    func toColor() -> NSColor {
+        switch self {
+        case .Grass:
+            return NSColor.lightGreen
+        case .Woods:
+            return NSColor.darkGreen
+        case .Desert:
+            return NSColor.yellow
+        case .Tundra:
+            return NSColor.white
+        case .Mountain:
+            return NSColor.darkGray
+        case .Water:
+            return NSColor.darkBlue
+        default:
+            return NSColor.systemPink
+        }
+    }
+}
+
+
+class GameBoardCell {
+    var terrain: GameBoardCellTerrain
+    var selected: Bool
+    var view: HexView
+    
+//    var canSelect: Bool {
+//        get {
+//            switch self.terrain {
+//            case .Grass:
+//                    return true
+//            case .Woods:
+//                    return true
+//            case .Desert:
+//                    return true
+//            case .Water:
+//                    return true
+//            default:
+//                    return false
+//            }
+//        }
+//    }
+
+    init(terrain: GameBoardCellTerrain) {
+        self.terrain = terrain
+        self.selected = false
+        // self.view = NSImageView(frame: NSRect(x: 0, y: 0, width: CellWidth, height: CellHeight))
+        // self.view = NSView(frame: NSRect(x: 0, y: 0, width: CellWidth, height: CellHeight))
+        self.view = HexView(frame: NSRect(x: 0, y: 0, width: CellWidth, height: CellHeight))
+
+        self.view.color = terrain.toColor()
+
+//        self.view.wantsLayer = true
+//        self.view.layer?.backgroundColor = terrain.toColor().cgColor
+
+        deselectCell()
+    }
+
+    func selectCell() {
+        self.selected = true
+        // self.view.layer?.borderWidth = 3
+        // self.view.layer?.borderColor = NSColor.white.cgColor
+        // self.view.color = NSColor.white
+        self.view.selected = true
+        self.view.display()
+    }
+
+    func deselectCell() {
+        self.selected = false
+        // self.view.layer?.borderWidth = 1
+        // self.view.layer?.borderColor = NSColor.black.cgColor
+        // self.view.color = NSColor.green
+        self.view.selected = false
+        self.view.display()
+    }
+    
+    func setTerrain(terrain: GameBoardCellTerrain) {
+        self.terrain = terrain
+        self.view.color = terrain.toColor()
+        self.view.display()
+    }
+
+}
+
+
+class GameBoardRow {
+    var cells: [GameBoardCell]
+
+    init(initialRow: [GameBoardCellTerrain]) {
+        self.cells = []
+
+        for x in 0..<MaxCols {
+            if x < initialRow.count {
+                self.cells.append(GameBoardCell(terrain: initialRow[x]))
+            }
+            else {
+                self.cells.append(GameBoardCell(terrain: .Other))
+            }
+
+        }
+    }
+}
+
+
+
+class GameBoard {
+    var rows: [GameBoardRow]
+
+    init(initialBoard: [[GameBoardCellTerrain]]) {
+        self.rows = []
+
+        for y in 0..<MaxRows {
+            if y < initialBoard.count {
+                self.rows.append(GameBoardRow(initialRow: initialBoard[y]))
+            }
+            else {
+                self.rows.append(GameBoardRow(initialRow: []))
+            }
+            
+        }
+    }
+
+
+    func clearHighlight() {
+        for y in 0..<MaxRows {
+            for x in 0..<MaxCols {
+                if rows[y].cells[x].selected {
+                    rows[y].cells[x].deselectCell()
+                }
+            }
+        }
+    }
+
+
+    func highlightCell(x: Int, y: Int) {
+        rows[y].cells[x].selectCell()
+    }
+
+}
--- WarGameDesigner/ViewController.swift
+++ WarGameDesigner/ViewController.swift
@@ -8,13 +8,45 @@
 
 import Cocoa
 
+let MaxRows = 20
+let MaxCols = 23
+var CellHeight: CGFloat = 60
+var CellWidth: CGFloat = CellHeight * 0.866
+let Padding: CGFloat = 20
+
+let SelectColor = NSColor.white
+let unSelectColor = NSColor.black
+
+
 class ViewController: NSViewController {
+
+    @IBOutlet weak var BoardView: NSView!
+    @IBOutlet weak var SizeLabel: NSTextField!
+    @IBOutlet weak var ZoomOutButton: NSButton!
+    @IBOutlet weak var ZoomInButton: NSButton!
+    @IBOutlet weak var CurrentTerrainView: NSView!
+
+    var mainBoard = GameBoard(initialBoard: [[]])
+
+    var currentTerrain = GameBoardCellTerrain.Grass
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        SizeLabel.stringValue = "Size: \(CellHeight)"
+
+        BoardView.wantsLayer = true
+
+        BoardView.layer?.backgroundColor = NSColor.lightGray.cgColor
+
+        BoardView.layer?.borderWidth = 1
+        BoardView.layer?.borderColor = .black
+
+        drawCells()
     }
+
 
     override var representedObject: Any? {
         didSet {
@@ -22,6 +54,116 @@ class ViewController: NSViewController {
         }
     }
 
+
+    func drawCells() {
+        for y in 0..<MaxRows {
+            for x in 0..<MaxCols {
+                var newX = Padding + (CGFloat(x) * (CellWidth * 1.0))
+                if (y % 2 == 1) {
+                    newX = newX + (CellWidth / 2)
+                }
+
+                let newY = Padding + (CGFloat(y) * ((CellHeight * 0.7486)))
+
+                mainBoard.rows[y].cells[x].view.frame = NSRect(x: newX, y: newY, width: CellWidth, height: CellHeight)
+                mainBoard.rows[y].cells[x].view.tag = (y * 1000) + x
+
+                BoardView.addSubview(mainBoard.rows[y].cells[x].view)
+
+                let tapGesture = NSClickGestureRecognizer()
+                tapGesture.target = self
+                tapGesture.buttonMask = 0x1   // left button
+                tapGesture.numberOfClicksRequired = 1
+                tapGesture.action = #selector(self.click(g:))
+                mainBoard.rows[y].cells[x].view.addGestureRecognizer(tapGesture)
+            }
+        }
+
+    }
+
+
+    @objc func click(g:NSGestureRecognizer) {
+        if let v = g.view as? HexView {
+
+            let touchpoint = g.location(in: v)
+
+            if v.insideMask(x: touchpoint.x, y: touchpoint.y) {
+                // print("Inside Hexagon : \(v.tag)")
+
+                let y: Int = v.tag / 1000
+                let x: Int = v.tag % 1000
+
+                mainBoard.rows[y].cells[x].setTerrain(terrain: currentTerrain)
+
+//                if (mainBoard.rows[y].cells[x].selected) {
+//                    mainBoard.rows[y].cells[x].deselectCell()
+//                }
+//                else {
+//                    mainBoard.clearHighlight()
+//                    mainBoard.rows[y].cells[x].selectCell()
+//                }
+
+            }
+            else {
+    //                print("Outside Hexagon")
+            }
+
+        }
+    }
+
+
+    @IBAction func ZoomOutClick(_ sender: Any) {
+        CellHeight = CellHeight * 0.5
+        CellWidth = CellHeight * 0.866
+        drawCells()
+        SizeLabel.stringValue = "Size: \(CellHeight)"
+        self.view.needsDisplay = true
+    }
+
+
+    @IBAction func ZoomInClick(_ sender: Any) {
+        CellHeight = CellHeight * 2.0
+        CellWidth = CellHeight * 0.866
+        drawCells()
+        SizeLabel.stringValue = "Size: \(CellHeight)"
+        self.view.needsDisplay = true
+    }
+
+
+    @IBAction func GrassButtonClick(_ sender: Any) {
+        currentTerrain = .Grass
+        CurrentTerrainView.layer?.backgroundColor = currentTerrain.toColor().cgColor
+    }
+
+
+    @IBAction func WoodsButtonClick(_ sender: Any) {
+        currentTerrain = .Woods
+        CurrentTerrainView.layer?.backgroundColor = currentTerrain.toColor().cgColor
+    }
+
+
+    @IBAction func WaterButtonClick(_ sender: Any) {
+        currentTerrain = .Water
+        CurrentTerrainView.layer?.backgroundColor = currentTerrain.toColor().cgColor
+    }
+
+
+    @IBAction func DesertButtonClick(_ sender: Any) {
+        currentTerrain = .Desert
+        CurrentTerrainView.layer?.backgroundColor = currentTerrain.toColor().cgColor
+    }
+
+
+    @IBAction func TundraButtonClick(_ sender: Any) {
+        currentTerrain = .Tundra
+        CurrentTerrainView.layer?.backgroundColor = currentTerrain.toColor().cgColor
+    }
+
+
+    @IBAction func MountainButtonClick(_ sender: Any) {
+        currentTerrain = .Mountain
+        CurrentTerrainView.layer?.backgroundColor = currentTerrain.toColor().cgColor
+    }
 
 }
 
